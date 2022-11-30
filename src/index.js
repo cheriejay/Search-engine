5d0160702c6c5fcc8a0cedb3b3a3b788f55e52d3
Jah <cjakpuche@gmail.com>
2022-11-30 16:25:12 +0100

    created index.js for file search engine

---
+++ src/index.js
@@ -0,0 +1,85 @@
+//current day and time
+let now = new Date();
+let currentDate = document.querySelector("#date");
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+let day = days[now.getDay()];
+let date = now.getDate();
+let months = [
+  "January",
+  "Febuary",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+let month = months[now.getMonth()];
+let hour = now.getHours();
+if (hour < 10) {
+  hour = `0${hour}`;
+}
+let minute = now.getMinutes();
+if (minute < 10) {
+  minute = `0${minute}`;
+}
+currentDate.innerHTML = `${day}, ${date} ${month}. ${hour}:${minute}`;
+
+//Replace the default city with the search value
+function search(city) {
+  let apiKey = "515c9ddbeb3cda9061acfab71031839e";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(showWeather);
+}
+
+function searchCity(event) {
+  event.preventDefault();
+  let city = document.querySelector("#search-in").value;
+  search(city);
+}
+let theCity = document.querySelector("#search-form");
+theCity.addEventListener("submit", searchCity);
+let button = document.querySelector("#s-button");
+button.addEventListener("click", searchCity);
+
+function showWeather(response) {
+  document.querySelector("h1").innerHTML = response.data.name;
+  document.querySelector("#desc").innerHTML =
+    response.data.weather[0].description;
+  document.querySelector("#temp").innerHTML = Math.round(
+    response.data.main.temp
+  );
+  document.querySelector("#pressure").innerHTML = response.data.main.pressure;
+  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+  document.querySelector("#wind").innerHTML = Math.round(
+    response.data.wind.speed
+  );
+}
+function currentLocation(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(theLocation);
+}
+function theLocation(position) {
+  let lat = position.coords.latitude;
+  let long = position.coords.longitude;
+  let apiKey = "0dc40d3d7cda209ca40e77430c74cf57";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(showWeather);
+}
+
+let sbutton = document.querySelector("#c-button");
+sbutton.addEventListener("click", currentLocation);
+
+search("Enugu");
