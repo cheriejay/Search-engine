e3ce6b45b327248fe1ae056bdbd8fd408899d717
Jah <cjakpuche@gmail.com>
2022-12-12 12:24:52 +0100

    updated the js file

--- src/index.js
+++ src/index.js
@@ -66,6 +66,11 @@ function showWeather(response) {
   document.querySelector("#wind").innerHTML = Math.round(
     response.data.wind.speed
   );
+  let icon = document.querySelector("#icon");
+  icon.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
 }
 function currentLocation(event) {
   event.preventDefault();
